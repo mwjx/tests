44ab7e13d27f1dac1491a0789ea664e01b107e5e
mwjx <liang0735@gmail.com>
2013-04-09 13:07:33 +0800

    aa

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
-#include <stdio.h>
+//#include <stdio.h>
+#include <iostream>  
+#include <time.h>  
+#include <stdlib.h>  
+using std::cout;  
+const int SIZE_CHAR = 32; //生成32 + 1位C Style字符串  
+const char CCH[] = "_0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";  
+int main()  
+{  
+    srand((unsigned)time(NULL));  
+    char ch[SIZE_CHAR + 1] = {0};  
+    for (int i = 0; i < SIZE_CHAR; ++i)  
+    {  
+       int x = rand()%sizeof(CCH);     
+       ch[i] = CCH[x];  
+    }  
+    cout <<ch <<"/n";        
+    return 0;  
+}  
+
 
 void ques()
 {
@@ -45,10 +64,31 @@ void click()
 //洗牌
 //改变运行状态.遍历座位表
 //踢出
-int main(int argc, char *argv[])
+/*int main(int argc, char *argv[])
 {
 	printf("Hello, world\n");
 	
 	return 0;
 }
+*/
+/*
+#include <iostream>  
+#include <string>  
+#include <vector>  
+using namespace std;  
+int main()  
+{  
+    string tmp1,tmp2;  
+    tmp1="rusher;lewjan;bill;stan;yx;";  
+    tmp2="abcc";  
+    vector<string>list;  
+    list.push_back(tmp1);  
+    list.push_back(tmp2);  
+    for (unsigned int i=0;i<list.size();i++)  
+    {  
+       cout<<list[i]<<endl;  
+    }  
+    return 0;  
+}  
+*/	
 
