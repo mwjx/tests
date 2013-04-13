239d453a261ae70925fe789c590f019d727f9cb1
mwjx <liang0735@gmail.com>
2013-04-13 15:13:31 +0800

    main.js

--- xfc/main.js
+++ xfc/main.js
@@ -2,3 +2,19 @@ window.onload = function()
 {
 	alert("hello");
 }
+
+//-------------
+var m_taskmg = function()
+{
+var arrtk = []; //[obj,...]
+function add()
+{
+}
+function run()
+{
+}
+return {
+run:run,
+add:add
+};
+}();
