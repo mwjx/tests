46a9c669625a380be99a3cdc168b80f6ec3df090
mwjx <liang0735@gmail.com>
2013-04-18 08:45:31 +0800

    aa

---
+++ feen.js
@@ -0,0 +1,389 @@
+﻿//------------------------------
+//create time:2013-3-29
+//creater:zll,liang_0735@21cn.com,www.mwjx.com
+//purpose:游戏界面
+//------------------------------
+xinclude("view/feen.js",["view/fe.js","view/visits.js","view/stline.js","ctrl/taskmg.js"],"m_feen.init");
+var m_feen = function(){
+var dir = m_cofm.get_dimg();
+var warea = 500,harea = 400; //480; //战场宽高
+var rtx =0,rty = 0; //右顶点坐标
+var wsm = MDEF["wsm"],hsm = MDEF["hsm"]; //640; //300; //小屏宽高
+var btnstart = null; //开始按钮 
+var mp_che = {}; //数据缓存:{w,h,ltx,lty}
+var arropt = []; //选项列表
+var pname1 = ["c1", "c2", "c3", "c4"]; //答对的反馈图案
+var pname2 = ["w1", "w2", "w3", "w4"]; //答错的反馈图案
+//"home/background.jpg"=>{id,w,h,x,y,evt[cmd,arg],sty,big,bigimg}
+var mp_img = {}; 
+//mp_img["en/bg_480_320.jpg"] = {"w":480,"h":320,"x":0,"y":0,"bigimg":"en/background.jpg","big":{"w":500,"h":400,"x":0,"y":0}}
+mp_img["en/start.jpg"] = {"id":"divstart","w":97,"h":33,"x":370,"y":260,"big":{"x":0,"y":0},"evt":[432,0],"sty":"cursor:pointer;"}
+mp_img["en/ppt_2.gif"] = {"id":"divhelp","w":24,"h":24,"x":330,"y":265,"evt":[434,0],"big":{"x":0,"y":0},"sty":"cursor:pointer;"}
+var mp_div = {}; //id=>{x,y}
+mp_div["info1"] = {"x":410,"y":10,"txt":"积分"};
+mp_div["info2"] = {"x":430,"y":30,"txt":"0"};
+mp_div["info3"] = {"x":410,"y":50,"txt":"题号"};
+mp_div["info4"] = {"x":430,"y":70,"txt":"0"};
+var mp_sty = {}; //id=>{x,y}
+mp_sty["info5"] = {"x":63,"y":117,"txt":""};
+mp_sty["item_area"] = {"x":121,"y":63};
+mp_sty["opt1"] = {"x":73,"y":156};
+mp_sty["opt2"] = {"x":73,"y":192};
+mp_sty["opt3"] = {"x":73,"y":229};
+mp_sty["opt4"] = {"x":73,"y":265};
+mp_sty["feedback"] = {"x":124,"y":125};
+mp_sty["end"] = {"x":100,"y":120};
+
+function init()
+{
+	g_trace("feen.js,初始");
+	make_face();
+	btnstart = $$("divstart");
+	arropt.push($$("opt1").childNodes[0].childNodes[1]);
+	arropt.push($$("opt2").childNodes[0].childNodes[1]);
+	arropt.push($$("opt3").childNodes[0].childNodes[1]);
+	arropt.push($$("opt4").childNodes[0].childNodes[1]);
+	m_visits.set_xy(rtx,rty);
+	m_stline.load_data(mp_che["w"],mp_che["h"],mp_che["ltx"],mp_che["lty"]);
+	//log_trace();
+}
+function tests(){}
+function make_face()
+{
+	//生成界面
+	init_mp();
+	var sm = is_sm(); //是否小屏
+	var arr = [];	
+	var img,path;
+	var sm = is_sm(); //是否小屏
+	var ww = sm?wsm:warea; //比例
+	var hh = sm?hsm:harea;
+	var xy = get_xy();
+	rtx = xy[0] + ww;
+	rty = xy[1];
+	mp_che["w"] = ww;
+	mp_che["h"] = hh;
+	mp_che["ltx"] = xy[0];
+	mp_che["lty"] = xy[1];
+	path = (dir+(sm?"en/bg_480_320.jpg":"en/background.jpg"));
+	arr.push("<DIV id=\"full_en\" style=\"position:absolute;left:"+xy[0]+"px;top:"+xy[1]+"px;width:"+ww+"px;height:"+hh+"px;BACKGROUND: url("+path+") no-repeat;z-index:0;\">");
+	arr.push("<div id=\"holder\" style=\"position:relative;left:"+(0)+"px;top:"+(0)+"px;width:"+ww+"px;height:"+hh+"px;\">");
+	arr.push("</div>");
+	for(var k in mp_img){
+		if(!sm && null!=mp_img[k]["bigimg"]){ //大屏
+			path = mp_img[k]["bigimg"];
+		}
+		else{
+			path = k;
+		}
+		arr.push(get_div({"img":k,"path":path,"w":mp_img[k].w,"h":mp_img[k].h,"x":mp_img[k].x,"y":mp_img[k].y}));
+	}
+	for(var k in mp_div){
+		arr.push(html_div(k));
+	}
+	//arr.push("<div id=info5 style=\"POSITION:absolute;LEFT:63px;TOP:117px;\"><font color=\"#80FF80\" size=\"4\"><span align=\"center\">上面单词的词义，最接近下面哪一个选项?</span></font></div>");
+	arr.push("<div id=info5 style=\"POSITION:absolute;LEFT:"+mp_sty["info5"].x+"px;TOP:"+mp_sty["info5"].y+"px;\"><font color=\"#80FF80\" size=\"4\"><span align=\"center\">上面单词的词义，最接近下面哪一个选项?</span></font></div>");
+	arr.push("<div id=item_area style=\"POSITION:absolute;LEFT:"+mp_sty["item_area"].x+"px;TOP:"+mp_sty["item_area"].y+"px;width:258px;visibility:hidden;\"><font color=\"#FF0000\" size=\"8\"><span align=\"center\">BADINAGE!</span></font></div>");
+
+	arr.push("<div id=opt1 style=\"POSITION:absolute;LEFT:"+mp_sty["opt1"].x+"px;TOP:"+mp_sty["opt1"].y+"px;width:258;visibility:hidden;\" onclick=\"g_ce().notify(433,0);\"><font color=\"#FFFF00\" size=\"4\">A. <span>开玩笑!</span></font></div>");
+	arr.push("<div id=opt2 style=\"POSITION:absolute;LEFT:"+mp_sty["opt2"].x+"px;TOP:"+mp_sty["opt2"].y+"px;width:258;visibility:hidden;\" onclick=\"g_ce().notify(433,1);\"><font color=\"#FFFF00\" size=\"4\">B. <span>开玩笑!</span></font></div>");
+	arr.push("<div id=opt3 style=\"POSITION:absolute;LEFT:"+mp_sty["opt3"].x+"px;TOP:"+mp_sty["opt3"].y+"px;width:258;visibility:hidden;\" onclick=\"g_ce().notify(433,2);\"><font color=\"#FFFF00\" size=\"4\">C. <span>开玩笑!</span></font></div>");
+	arr.push("<div id=opt4 style=\"POSITION:absolute;LEFT:"+mp_sty["opt4"].x+"px;TOP:"+mp_sty["opt4"].y+"px;width:258;visibility:hidden;\" onclick=\"g_ce().notify(433,3);\"><font color=\"#FFFF00\" size=\"4\">D. <span>开玩笑!</span></font></div>");
+
+	arr.push("<div id=feedback style=\"POSITION:absolute;LEFT:"+mp_sty["feedback"].x+"px;TOP:"+mp_sty["feedback"].y+"px;width:258;visibility:hidden;\"><img src=\"images/en/c1.gif\" width=\"255\" height=\"170\"></div>")
+	arr.push("<div id=end style=\"POSITION:ABSOLUTE;LEFT:"+mp_sty["feedback"].x+"PX;TOP:"+mp_sty["feedback"].y+"PX;width:300;visibility:hidden;z-index:5;filter:shadow(color=silver,direction=900,enabled=1)\"><h1><font color=\"#0000FF\" size=\"12\"><p id=s4 align=\"center\">Game Over!</p></font></h1></div>");
+
+	arr.push("</DIV>");
+	m_fe.face.insertAdjacentHTML("beforeend",arr.join(""));
+}
+function show_fb(flag)
+{
+	//显示结果
+	//输入:flag:true对/false错
+	//输出:无
+	var i = parseInt(Math.random() * 4);
+	var path = (dir+"en/"+(flag?pname1[i]:pname2[i])+".gif");
+	var o = $$("feedback").childNodes[0];
+	o.src = path;
+	opendiv("feedback",true);
+}
+function open_info(flag)
+{
+	//开关题号项
+	//输入:flag:true/false
+	//输出:无
+	for(var k in mp_div){
+		$$(k).style.display = (flag?"block":"none");
+	}
+}
+function html_div(id)
+{
+	//display:none;
+	return ("<div id=\""+id+"\" style=\"POSITION:ABSOLUTE;LEFT:"+mp_div[id].x+"PX;TOP:"+mp_div[id].y+"PX;\"><h3><font color=\"#FFFF00\"><span>"+mp_div[id].txt+"</span></font></h3></div>");
+}
+function up_end(val)
+{
+	//更新结束
+	//输入:val值
+	//输出:无
+	var o = $$("end").childNodes[0].childNodes[0].childNodes[0];
+	o.innerText = val;
+}
+function up_title(val)
+{
+	//更新题目
+	//输入:val值
+	//输出:无
+	//glog("up_title,val="+val);
+	var o = $$("item_area").childNodes[0].childNodes[0];
+	o.innerText = val;
+	//glog("o.innerText="+o.innerText);
+	//alert($$("item_area").innerHTML);
+}
+function light(no,c)
+{
+	//高亮选项
+	//输入:no下标:0-3,c:red,green
+	//输出:无
+	if(!c){c="red";}
+	var o = arropt[no].parentNode;
+	o.style.color=c;
+}
+function up_opt(arr)
+{
+	//更新选项
+	//输入:arr:[选项,...]
+	//输出:无
+	assert.call(this,arr.length==arropt.length);
+	//glog("up_opt="+arr.length+","+arropt.length);
+	var o = null;
+	for(var i = 0;i<arr.length;++i){
+		arropt[i].innerText = arr[i];
+		o = arropt[i].parentNode;
+		o.style.color="#FFFF00";
+	}
+}
+function up_no(val)
+{
+	//更新题号
+	//输入:val值
+	//输出:无
+	var o = $$("info4").childNodes[0].childNodes[0].childNodes[0];
+	o.innerText = (""+val);
+}
+function up_score(val)
+{
+	//更新分数
+	//输入:val值
+	//输出:无
+	var o = $$("info2").childNodes[0].childNodes[0].childNodes[0];
+	o.innerText = (""+val);
+}
+function opendiv(id,flag)
+{
+	//开始元素
+	//输入:id编号,flag:true开/false关
+	//输出:无
+	$$(id).style.visibility= (flag?"visible":"hidden");
+}
+function sw_start(flag)
+{
+	//切换开关图
+	//输入:flag:true开/false关
+	//输出:无
+	var o = btnstart.childNodes[0];
+	o.src=(dir+(flag?"en/start.jpg":"en/stop.jpg"));
+}
+function init_mp()
+{
+	//初始数据
+	var sm = is_sm(); //是否小屏
+	var ww = wsm/warea; //比例
+	var hh = hsm/harea;
+	var xy = get_xy();
+	for(var k in mp_img){
+		if(!sm && mp_img[k]["big"]){ //大屏
+			//wh
+			if(mp_img[k]["big"]["w"]){
+				mp_img[k].w = mp_img[k]["big"].w;
+				mp_img[k].h = mp_img[k]["big"].h;
+			}
+			//xy
+			if(mp_img[k]["big"]["x"]){
+				mp_img[k].x = mp_img[k]["big"].x;
+				mp_img[k].y = mp_img[k]["big"].y;
+			}
+		}
+//		mp_img[k].x += xy[0];
+//		mp_img[k].y += xy[1];
+	}
+//	for(var k in mp_div){
+//		mp_div[k].x += xy[0];
+//		mp_div[k].y += xy[1];
+//	}
+//	for(var k in mp_sty){
+//		mp_sty[k].x += xy[0];
+//		mp_sty[k].y += xy[1];
+//	}
+}
+function get_div(info)
+{
+	//图层
+	//输入:{img,w,h,x,y,path}
+	//输出:字符串
+	var evt = "",sty="",id="";
+	//return "";
+	if(null!=mp_img[info.img]["id"]){
+		id = "id=\""+mp_img[info.img]["id"]+"\"";
+	}
+	if(null!=mp_img[info.img]["evt"]){
+		evt = ("onclick=\"javascript:g_ce().notify("+mp_img[info.img]["evt"][0]+","+mp_img[info.img]["evt"][1]+");\"");
+	}
+	if(null!=mp_img[info.img]["sty"]){
+		sty = (mp_img[info.img]["sty"]);
+	}
+	return ("<div "+id+" style=\"POSITION:ABSOLUTE;TOP:"+info.y+"PX;LEFT:"+info.x+"PX;"+sty+"\" "+evt+"><img border=\"0\" src=\""+dir+info.path+"\" width=\""+info.w+"\" height=\""+info.h+"\"></div>");
+}
+function is_sm(){return true;return (m_coagent.get_w()<warea || m_coagent.get_h()<harea);}
+function get_xy()
+{
+	//层起始坐标
+	//输入:无
+	//输出:[x,y]
+	var ret = [0,0];
+	//return ret;
+	//屏幕宽高
+	var w = m_coagent.get_w();
+	var h = m_coagent.get_h();
+	var sm = is_sm(); //是否小屏
+	var ww = sm?MDEF["wsm"]:warea; //对应屏宽高
+	var hh = sm?MDEF["hsm"]:harea;
+	//glog("w="+w+",h="+h);
+	var x = parseInt((w-ww)/2,10);
+	var y = parseInt((h-hh)/2,10);
+	if(x>=0 && y>=0){ret = [x,y];}
+	return ret;
+}
+function tip(txt)
+{
+	//提示
+	//参数:txt内容
+	//返回:无
+	opendiv("end",true);
+	up_end(txt);    
+	//弹跳任务
+	//关闭任务,3秒后
+	m_taskmg.add(new c_tkcloseend());
+}
+return {
+	init:init,
+	opendiv:opendiv, //开关元素
+	sw_start:sw_start, //开始图
+	up_score:up_score, //更新分数
+	up_no:up_no, //更新题号
+	up_title:up_title, //新新题目
+	up_opt:up_opt, //更新选项
+	up_end:up_end, //更新结束
+	show_fb:show_fb, //显示结果
+	light:light, //高亮选项
+	open_info:open_info, //开关题号项
+	tip:tip, //提示
+	tests:tests
+};
+}();
+
+//弹跳任务
+function c_tkcloseend()
+{
+//关提示任务
+//参数:无
+//返回:无
+var blnover = false; //是否结果
+var count = 90; //N帧后关闭
+//var sit = v;
+//var nodes = arr; //节点表
+//var lastx = nodes[0][0];
+//var lasty = nodes[0][1];
+//var sx,sy,ex,ey;
+//var size = 25;
+//glog("c_tkline,sit="+sit);
+function over(){blnover=true;}
+function run()
+{
+	//画线
+	//参数:无
+	//返回:true完成/false继续
+	//glog("c_tkline,run,len="+nodes.length);
+	if(blnover){m_feen.opendiv("end",false);return true;}
+	--count;
+	if(count<1){m_feen.opendiv("end",false);return true;}
+	return false;
+}
+return {
+run:run,
+over:over //结束
+};
+}
+
+//关闭任务
+function c_tkcloseend()
+{
+//关提示任务
+//参数:无
+//返回:无
+var blnover = false; //是否结果
+var count = 90; //N帧后关闭
+//var sit = v;
+//var nodes = arr; //节点表
+//var lastx = nodes[0][0];
+//var lasty = nodes[0][1];
+//var sx,sy,ex,ey;
+//var size = 25;
+//glog("c_tkline,sit="+sit);
+function over(){blnover=true;}
+function run()
+{
+	//画线
+	//参数:无
+	//返回:true完成/false继续
+	//glog("c_tkline,run,len="+nodes.length);
+	if(blnover){m_feen.opendiv("end",false);return true;}
+	--count;
+	if(count<1){m_feen.opendiv("end",false);return true;}
+	return false;
+	/*
+	if(nodes.length<1){return true;}
+	//var k = nodes.length-1;
+	var nx = nodes[0][0]; //下一个节点
+	var ny = nodes[0][1];
+	var x = nx,y = ny;
+	if(lastx!=nx){
+		x = (lastx<nx)?(lastx+size):(lastx-size);
+		x = (Math.abs(nx-x)<=size)?nx:x;
+	}
+	if(lasty!=ny){
+		y = (lasty<ny)?(lasty+size):(lasty-size);
+		y = (Math.abs(ny-y)<=size)?ny:y;
+	}
+	//glog(x+","+y+";"+nx+","+ny);
+	sx = lastx;
+	sy = lasty;
+	ex = x;
+	ey = y;
+	lastx = x;
+	lasty = y;
+	if(nx==x && ny==y){
+		nodes.shift();
+		if(nodes.length<1){g_ce().notify(439,[sit,nx,ny]);}
+	}
+	m_stline.draw(sit,sx,sy,ex,ey);
+	return false;
+	*/
+}
+return {
+run:run,
+over:over //结束
+};
+}
+
+function exists_feen(){return true;} //确认函数
---
+++ moen.js
@@ -0,0 +1,245 @@
+﻿//------------------------------
+//create time:2013-3-30
+//creater:zll,liang_0735@21cn.com,www.mwjx.com
+//purpose:英语逻辑
+//------------------------------
+xinclude("model/moen.js",[],"m_moen.init");
+var m_moen = function(){
+var wordlist;
+var game_status=0; //当前状态:0结束/1开始
+var allno = 80,optnum=4; //词条数,选项数
+var total_items=5,item_no,p_answerno=-1,p_score;
+var p_wordno = -1; //词号:0-?
+var itemlist=[];
+for(var i=0;i<total_items;i++){
+	itemlist[i]=[];
+}
+var pname1 = ["c1", "c2", "c3", "c4"]; //答对的反馈图案
+var pname2 = ["w1", "w2", "w3", "w4"]; //答错的反馈图案
+var clist=[],wlist=[];
+
+
+function init()
+{
+	g_trace("moen.js,初始");
+	init_wordlist();
+
+	//log_trace();
+}
+function tests(){}
+function get_state()
+{
+	//全部状态
+	//输入:无
+	//输出:[状态,题号,分数,未结束]
+	return [game_status,item_no,p_score,item_no<total_items];
+}
+function start_game()
+{
+	//游戏开始
+	var i;
+	switch(game_status){
+	case 0: //start  开始游戏   
+		//glog("start_game,start");
+		p_score=0; //分数
+		item_no=0; //题号
+		game_status=1; //状态
+		gene_items();
+		g_ce().notify(547,0); //开始界面
+		break;
+	case 1: //stop  停止游戏
+		game_status=0;
+		g_ce().notify(548,0); //结束界面
+		break;
+	}
+}
+function gene_items()
+{
+	// 随机产生题组编号
+	//glog("gene_items");
+	var i, j, x, xlist=[];
+	for(i=0;i<allno;i++){
+		xlist[i] = i;
+	}
+	//随机排列数组数据
+	for(i=0;i<allno;i++){
+		j = parseInt(Math.random() * 80);
+		x = xlist[i];
+		xlist[i] = xlist[j];
+		xlist[j] = x;
+	}
+	//产生题目数组
+	for(i=0;i<total_items;i++){
+		itemlist[i][0] = xlist[i * 4];
+		itemlist[i][1] = xlist[i * 4 + 1];
+		itemlist[i][2] = xlist[i * 4 + 2];
+		itemlist[i][3] = xlist[i * 4 + 3];
+	}
+}
+function titlebyno(no)
+{
+	//根据词号取题目
+	//输入:no词号
+	//输出:题目
+	return wordlist[no][0];
+}
+function chbyno(no)
+{
+	//根据词号取中文
+	//输入:no词号
+	//输出:中文
+	return ((no>=0)?wordlist[no][2]:"");
+}
+function new_item()
+{
+	//新组
+	//输入:无
+	//输出:[题号,问题,[选项表,...]]
+	p_answerno = parseInt(Math.random() * optnum);
+	p_wordno = itemlist[item_no][p_answerno]; //词号
+	var ret = [item_no+1,wordlist[p_wordno][0],[]];
+	for(var i=0;i<optnum;i++){
+		ret[2].push(wordlist[itemlist[item_no][i]][2]);
+	}
+	return ret;
+}
+function item_click(index)
+{
+	//处理答案的程序
+	//输入:index:0-3
+	//输出:true正确/false错误
+	if(item_no>=total_items){return false;}
+	var ret = (index == p_answerno);
+	if(ret){ //正确答案
+		p_score = p_score + 10;
+		if(p_wordno>=0){g_ce().notify(549,[0,p_wordno]);}
+	} 
+	item_no = item_no + 1;
+	return ret;
+}
+function had_click()
+{
+	//能否选中
+	//输入:无
+	//输出:true/false
+	if(!is_ing()){return false;}
+	if(item_no>=total_items){return false;}
+	return true;
+}
+function help()
+{
+	//正确答案
+	//输入:无
+	//输出:-1失败,0-3
+	if(!had_click()){return -1;}
+	if(p_answerno<0){return -1;}
+	p_wordno = -1; //去掉词号,保证不能进度存档
+	return p_answerno;
+}
+function init_wordlist()
+{
+	// 设置题目数组
+	var word0 = new Array("abase", "lower; humiliate", "降低；屈辱");
+	var word1 = new Array("abash", "embarrass", "使困窘");
+	var word2 = new Array("abbreviate", "shorten", "缩短");
+	var word3 = new Array("abdicate", "renounce; give up", "弃绝；放弃");
+	var word4 = new Array("aberration", "wandering or straying; in optics, fa", "游荡；漂泊");
+	var word5 = new Array("abettor", "encourager", "教唆者");
+	var word6 = new Array("abeyance", "suspended action", "暂缓");
+	var word7 = new Array("abhor", "detest; hate", "憎恨");
+	var word8 = new Array("abjure", "renounce upon oath", "誓言放弃");
+	var word9 = new Array("ablution", "washing", "沐浴");
+	var word10 = new Array("abnegation", "repudiation; self-sacrifice", "否认；自我牺牲");
+	var word11 = new Array("abolish", "cancel; put an end to", "废除");
+	var word12 = new Array("abominate", "loathe; hate", "憎恶");
+	var word13 = new Array("aboriginal", "being the first of its kind in a reg", "原始的；本土的");
+	var word14 = new Array("abortive", "unsuccessful; fruitless", "未成功的；发育不全的");
+	var word15 = new Array("abrade", "wear away by friction; erode", "磨损");
+	var word16 = new Array("abridge", "condense or shorten", "缩短；删节");
+	var word17 = new Array("abrogate", "abolish", "废止；废除");
+	var word18 = new Array("abscond", "depart secretly and hide", "秘密潜逃；藏匿");
+	var word19 = new Array("absolve", "pardon (an offense);", "赦免");
+	var word20 = new Array("abstemious", "sparing in eating and drinking; temp", "戒酒；节制");
+	var word21 = new Array("abstinence", "restrain from eating or drinking", "饮食有度的；适度的");
+	var word22 = new Array("abstract", "theoretical; not concrete; nonrepres", "抽象的；难了解的");
+	var word23 = new Array("abstruse", "obscure; profound; difficult to unde", "深奥的；难解的");
+	var word24 = new Array("abusive", "coarsely insulting; physically harmf", "虐待的；辱骂的");
+	var word25 = new Array("abut", "border upon; adjoin", "接界；毗连");
+	var word26 = new Array("abysmal", "bottomless", "无底的；地狱的");
+	var word27 = new Array("accede", "agree", "同意");
+	var word28 = new Array("accelerate", "move faster", "加速");
+	var word29 = new Array("accessible", "easy to approach; obtainable", "易达到的；可获得的");
+	var word30 = new Array("accessory", "additional object; useful but not es", "附属品；配件；从犯");
+	var word31 = new Array("acclimate", "adjust to climate", "使适应於");
+	var word32 = new Array("acclivity", "sharp upslope of a hill", "向上的斜坡");
+	var word33 = new Array("babble", "chatter idly", "多嘴；说话模糊不清");
+	var word34 = new Array("bacchanalian", "drunken", "发酒疯的");
+	var word35 = new Array("badger", "pester; annoy", "困扰");
+	var word36 = new Array("badinage", "teasing conversation", "嘲弄；开玩笑");
+	var word37 = new Array("baffle", "frustrate; perplex", "使困惑；阻碍");
+	var word38 = new Array("bagatelle", "trifle", "琐事");
+	var word39 = new Array("baleful", "deadly; destructive", "致命的");
+	var word40 = new Array("bait", "harass;tease", "迫之使怒；逗弄");
+	var word41 = new Array("balk", "foil", "阻碍；错过");
+	var word42 = new Array("balm", "something that relieves pain", "止痛的东西；慰藉");
+	var word43 = new Array("balmy", "mild; fragrant", "温和的；芳香的");
+	var word44 = new Array("banal", "hackneyed; commonplace; trite", "平凡的；琐屑的");
+	var word45 = new Array("bandanna", "large, bright-colored handkerchief", "大手帕；大　巾");
+	var word46 = new Array("cabal", "small group of persons secretly unit", "阴谋；派系");
+	var word47 = new Array("cache", "hiding place", "地窖");
+	var word48 = new Array("cacophony", "discord", "刺耳的声音");
+	var word49 = new Array("cadaver", "corpse", "尸体");
+	var word50 = new Array("cadaverous", "like a corpse; pale", "似尸的；憔悴的");
+	var word51 = new Array("cajole", "coax; wheedle", "甘言诱惑");
+	var word52 = new Array("calamity", "disaster; misery", "灾难");
+	var word53 = new Array("caliber", "ability; capacity", "口径；才干");
+	var word54 = new Array("calligraphy", "beautiful writing; excellent penmans", "书法");
+	var word55 = new Array("callous", "hardened; unfeeling", "无情的");
+	var word56 = new Array("callow", "youthful; immature", "无经验的；未生羽毛的");
+	var word57 = new Array("calorific", "heat-producing", "生熟的；发热的");
+	var word58 = new Array("calumniate", "slander", "诽谤");
+	var word59 = new Array("camaraderie", "good-fellowship", "同志爱；友谊");
+	var word60 = new Array("cameo", "shell or jewel carved in relief", "刻有浮雕之宝石");
+	var word61 = new Array("canard", "unfounded rumor; exaggerated report", "虚报");
+	var word62 = new Array("candor", "frankness", "诚恳");
+	var word63 = new Array("canine", "related to dogs; dog-like", "犬科动物");
+	var word64 = new Array("canker", "any ulcerous sore; any evil", "积病");
+	var word65 = new Array("canny", "shrewd; thrifty", "灵敏的；小心的");
+	var word66 = new Array("cant", "jargon of thieves; pious phraseology", "伪善之言；隐语");
+	var word67 = new Array("cantankerous", "ill humored; irritable", "难处的");
+	var word68 = new Array("cantata", "story set to music, to be sung by a", "清唱剧");
+	var word69 = new Array("canter", "slow gallop", "慢跑");
+	var word70 = new Array("canto", "division of a long poem", "长诗中的篇");
+	var word71 = new Array("canvass", "determine votes, etc", "讨论；拉票");
+	var word72 = new Array("capacious", "spacious", "宽广的");
+	var word73 = new Array("caparison", "showy harness or ornamentation for a", "打扮；马的装饰物");
+	var word74 = new Array("capillary", "having a very fine bore", "毛细管的；毛状的");
+	var word75 = new Array("capitulate", "surrender", "有条件的投降");
+	var word76 = new Array("caprice", "whim", "反覆无常；幻想");
+	var word77 = new Array("capricious", "fickle; incalculable", "多变的");
+	var word78 = new Array("caption", "title; chapter heading; text under i", "标题；插图的说明");
+	var word79 = new Array("captious", "faultfinding", "爱找岔子的，强词夺理的");
+	//将题目数据转换到wordlist数组中
+	var str1="wordlist=[";
+	//var i;
+	for(var i=0;i<79;i++){
+		str1=str1+"word"+i+",";
+	}
+	str1=str1+"word79]";
+	eval(str1);
+}
+function is_ing(){return (1==game_status);}
+return {
+	init:init,
+	start_game:start_game, //开始
+	get_state:get_state, //全部状态
+	new_item:new_item, //新组
+	is_ing:is_ing, //是否开始
+	item_click:item_click, //处理选中
+	help:help, //正确答案
+	titlebyno:titlebyno, //根据词号取题目
+	chbyno:chbyno, //根据词号取中文
+	tests:tests
+};
+}();
+function exists_moen(){return true;} //确认函数
