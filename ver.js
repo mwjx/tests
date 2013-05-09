a3381a0e9c6620c8d22c452ff0f98139d1f198f7
mwjx <liang_0735@163.com>
2013-05-09 17:49:10 +0800

    aa

---
+++ ver.js
@@ -0,0 +1,25 @@
+var m_ver = function()
+{
+var rootloc = "";
+var rootser = "";
+function init()
+{
+}
+function tests()
+{
+	up_dir("ftmp/");
+}
+function up_dir(d)
+{
+	//取本地列表
+	//取远程列表
+	//比较出新文件列表
+	//下载新文件
+	//更新本地列表文件
+	//目录递归
+}
+return {
+init:init,
+tests:tests
+};
+}();
